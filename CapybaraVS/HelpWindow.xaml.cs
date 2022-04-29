553b2c199a1177f65d065d8e946719bd94745af2
Katsumiar <katsumi.aradono@gmail.com>
2022-04-29 10:20:12 +0900

    バージョン変更及びパッケージ更新

--- CapybaraVS/HelpWindow.xaml.cs
+++ CapybaraVS/HelpWindow.xaml.cs
@@ -37,11 +37,12 @@ namespace CapyCSS
             Info.Text += Environment.NewLine;
             Info.Text += "Copyright notice:" + Environment.NewLine;
             Info.Text += "  Microsoft.NET " + Environment.Version.ToString() + " (" + (Environment.Is64BitProcess ? "64bit" : "32bit") + ")" + Environment.NewLine;
-            Info.Text += "  Material Design In XAML Toolkit 4.3.0" + Environment.NewLine;
-            Info.Text += "  Microsoft.ML.NET 1.7.0" + Environment.NewLine;
-            Info.Text += "  Microsoft.ML.ImageAnalytics 1.7.0" + Environment.NewLine;
-            Info.Text += "  Microsoft.ML.OnnxRuntime 1.10.0" + Environment.NewLine;
-            Info.Text += "  Microsoft.ML.OnnxTransformer 1.7.0" + Environment.NewLine;
+            Info.Text += "  Material Design In XAML Toolkit 4.5.0" + Environment.NewLine;
+            Info.Text += "  Microsoft.ML.NET 1.7.1" + Environment.NewLine;
+            Info.Text += "  Microsoft.ML.ImageAnalytics 1.7.1" + Environment.NewLine;
+            Info.Text += "  Microsoft.ML.OnnxRuntime 1.11.0" + Environment.NewLine;
+            Info.Text += "  Microsoft.ML.OnnxTransformer 1.7.1" + Environment.NewLine;
+            Info.Text += "  System.Drawing.Common 6.0.0" + Environment.NewLine;
 
             Info.Text += Environment.NewLine;
             Info.Text += "Shortcut Key:" + Environment.NewLine;
